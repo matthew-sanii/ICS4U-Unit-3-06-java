def78fc6bd31ccf3b5811825c443fd88d6ebaa2d
matthew-sanii <60710943+matthew-sanii@users.noreply.github.com>
2021-12-14 17:27:02 +0000

    edit

---
+++ Airplane.java
@@ -0,0 +1,15 @@
+public class Airplane {
+
+  private int speed;
+
+  public Airplane() {
+  }
+
+  public void setSpeed(int speed) {
+    this.speed = speed;
+  }
+
+  public int getSpeed() {
+    return speed;
+  }
+}
